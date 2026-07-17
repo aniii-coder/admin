8115fbb2191da63967c6011d2e5ebe8ccbd1f8cb
Aniket Singh <aksingh22356@gmail.com>
2026-07-17 23:55:46 +0530

    added auth admin api

---
+++ src/store.js
@@ -0,0 +1,11 @@
+// src/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { baseApi } from './services/baseApi';
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
